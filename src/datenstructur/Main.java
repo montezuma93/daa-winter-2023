83a0c84a936e21984b6d4c28201e2a888ef18f93
montezuma93 <hmr@riki5.de>
2024-02-02 12:02:43 +0100

    add line checker

--- src/datenstructur/Main.java
+++ src/datenstructur/Main.java
@@ -6,19 +6,26 @@ public class Main {
 
     public static void main(String[] args) {
         String[][] board = {
-                {"X", "0", "X"},
+                {"X", "-", "X"},
                 {"0", "-", "X"},
-                {"X", "0", "X"}
+                {"X", "X", "X"}
         };
 
         for(int i = 0; i< board.length; i++){
-            for(int j = 0; j < board[i].length -1; j++) {
-                System.out.println(i);
-                System.out.println(j);
-                System.out.println(board[i][j] +"und"+board[i][j+1]);
-                if (board[i][j] == board[i][j+1]) {
-
+            String charToRemember = null;
+            boolean lineHasWinner = true;
+            for(int j = 0; j < board[i].length; j++) {
+                System.out.println(board[i][j]);
+                if (charToRemember == null) {
+                    charToRemember = board[i][j];
+                } else if(charToRemember != board[i][j]){
+                    lineHasWinner = false;
+                    break;
                 }
+            }
+            if(lineHasWinner == true) {
+                System.out.println("line has winner");
+                break;
             }
         }
 
